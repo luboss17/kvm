2d3b840bbab28652cf8120125373b3ea34de6b9b
luboss17 <huyhuynh1228@gmail.com>
2019-10-23 23:35:26 -0700

    attempt write rax with hex

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -7397,6 +7397,9 @@ int kvm_emulate_hypercall(struct kvm_vcpu *vcpu)
 		kvm_sched_yield(vcpu->kvm, a0);
 		ret = 0;
 		break;
+	case KVM_CMPE283:
+		ret=0x0033383245504D43;
+		break;
 	default:
 		ret = -KVM_ENOSYS;
 		break;
